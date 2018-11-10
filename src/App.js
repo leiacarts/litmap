e7b161026b80316ffa92e0cf007a90a0240d8f2d
Leia Carter <leiacarts@gmail.com>
2018-11-10 04:23:15 -0500

    added map stylesheet

--- src/App.js
+++ src/App.js
@@ -45,12 +45,115 @@ class App extends Component {
 
   initMap = () => {
 
+    //map style
+    let styledMapType = new window.google.maps.StyledMapType(
+      [{
+        "featureType": "all",
+        "elementType": "geometry.fill",
+        "stylers": [{"weight": "2.00"}]
+      },
+      {
+        "featureType": "all",
+        "elementType": "geometry.stroke",
+        "stylers": [{"color": "#9c9c9c"}]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text",
+        "stylers": [{"visibility": "on"}]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [{"color": "#f2f2f2"}]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry.fill",
+        "stylers": [{"color": "#ffffff"}]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [{"color": "#ffffff"}]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [{"visibility": "off"}]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [{"saturation": -100}, {"lightness": 45}]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [{"color": "#eeeeee"}]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [{"color": "#7b7b7b"}]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [{"color": "#ffffff"}]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [{"visibility": "simplified"}]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [{"visibility": "off"}]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [{"visibility": "off"}]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [{"color": "#46bcec"}, {"visibility": "on"}]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [{"color": "#c8d7d4"}]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [{"color": "#070707"}]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [{"color": "#ffffff"}]
+    }
+],
+            {name: 'Styled Map'});
+
+
     //displays map in window
-    //initial coords @ nyc
+    //initial coords @ parsons nyc
     let map = new window.google.maps.Map(document.getElementById('map'), {
-        center: {lat: 40.7128, lng: -74.0060},
-        zoom: 12
+        center: {lat: 40.7353, lng: -73.9946},
+        zoom: 14,
+        mapTypeControlOptions: {
+            mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+                    'styled_map']
+          }
     })
+
+    map.mapTypes.set('styled_map', styledMapType);
+        map.setMapTypeId('styled_map');
 
     //creates infobox per marker
     let infowindow = new window.google.maps.InfoWindow()
